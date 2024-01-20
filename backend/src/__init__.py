19d3723a2dc8f7d9a46e9d191c391186df78737e
26fati <fatibeautiful1@gmail.com>
2024-01-20 01:10:41 +0100

    create user table

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -3,6 +3,7 @@ import os
 from src.config.config import Config
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # loading environment variables
 load_dotenv()
@@ -26,7 +27,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI_
 db = SQLAlchemy(app)
 
 # Flask Migrate instance to handle migrations
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 # import models to let the migrate tool know
 from src.models.user_model import User
