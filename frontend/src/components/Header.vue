26d9562b8a03cea6dc292ee75d3ab04e012cb1ef
26fati <fatibeautiful1@gmail.com>
2024-02-10 20:13:38 +0100

    add icon to header vue

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -13,21 +13,38 @@ const HandleLogout = () => {
 <template>
     <div class="navbar bg-base-100 shadow">
         <div class="w-2/4">
-            <router-link to="/dashboard" class="text-5xl font-bold text-primary">Sofpital</router-link>
+            <router-link to="/dashboard">
+            <a class="text-blue-500 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+              <!--Icon from: http://www.potlabicons.com/ -->
+              <!-- <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
+                <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
+                <path
+                  class="plane-take-off"
+                  d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
+                />
+              </svg> -->
+              <img src="../views/Screenshot_from_2024-02-10_14-57-26-removebg-preview (1).png" alt="Icon" style="width: 50px; height: 50px;" class="inline" />
+              Sofpital
+            </a></router-link>
+            <!-- </a>class="text-5xl font-bold text-primary">Sofpital</router-link> -->
         </div>
         <div class="flex-none gap-2">
             <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal px-1">
                     <li>
+                        
                         <router-link to="/dashboard">Dashboard</router-link>
                     </li>
                     <li>
+                        
                         <router-link to="/learner_posts">posts</router-link>
                     </li>
                     <li>
+                        
                         <router-link to="/learner_offers">offers</router-link>
                     </li>
                     <li>
+                        
                         <router-link to="/learner_meetings">meetings</router-link>
                     </li>
                 </ul>
@@ -51,3 +68,9 @@ const HandleLogout = () => {
         </div>
     </div>
 </template>
+
+<style scoped>
+.gradient {
+    background: linear-gradient(90deg, #daae51 10%, #1956b5 87%);
+}
+</style>
---
+++ frontend/src/components/swiper.vue
@@ -0,0 +1,58 @@
+<script>
+
+import 'swiper/swiper-bundle.css'
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.min.css'
+
+export default {
+    data() {
+      return {
+        images: [
+          { src: "./images/genie-1.jpg"},
+          { src: './images/genie-2.jpg'},
+          { src: 'path/to/image3.jpg' },
+        ],
+        swiperOption: {
+          slidesPerView: 1,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+          },
+          loop: true,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+        },
+      }
+    },
+  }
+</script>
+
+<template>
+    <div class="swiper-container">
+      <Swiper :options="swiperOption">
+        <SwiperSlide v-for="(image, index) in images" :key="index">
+          <img :src="image.src" alt="Slide Image">
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  </template>
+
+<style scoped>
+.swiper-container {
+  @apply  w-full h-64 md:h-96 xl:h-128;
+}
+
+.swiper-slide {
+  @apply h-full;
+}
+
+.swiper-slide img {
+  @apply w-full h-full object-cover;
+}
+
+.swiper-pagination {
+  @apply bottom-0 left-0 right-0;
+}
+</style>
