49099bdc046218b71f65fe64e1f99b291e487f3a
zineddine LOUZANI <31287591+louzanizineddine@users.noreply.github.com>
2024-02-22 17:13:22 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const apiURL = 'https://sofpital-api-a15300b6f672.herokuapp.com/api/'
-export const apiURL = 'http://localhost:8000/api/'
+export const apiURL = 'https://sofpital-api-a15300b6f672.herokuapp.com/api/'
+// export const apiURL = 'http://localhost:8000/api/'
